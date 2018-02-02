aba2b595bc6413bf8f1e75e610c957f5c5034992
Eden Mesfun <emesfun@gmail.com>
2018-02-02 20:03:28 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+require 'pry'
+
+class School
+  attr_accessor
+  attr_reader :grade, :name, :roster
+
+  def initialize(name)
+    @name=name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
+  end
+
+  def grade(grade)
+    @roster.fetch(grade)
+  end
+
+  def sort
+    sorted_list = {}
+    @roster.each do |student, grade|
+      sorted_list[grade] = student[sort]
+    end #each iteration
+    sorted_list
+  end #method
+
+end #class
